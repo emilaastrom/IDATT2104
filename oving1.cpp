3cdc5a0225df02b06fecd6cb5cdef79e16c0a36e
Emil Leonard Aastrøm <126675002+emilaastrom@users.noreply.github.com>
2024-02-09 11:31:15 +0100

    Commented out unused code, added thread-safe printing of prime numbers

--- oving1.cpp
+++ oving1.cpp
@@ -11,15 +11,17 @@ int sum = 0;
 mutex coutMutex;
 mutex sumMutex;
 
-void printPrimeNumber(int number, int threadId) {
+/* void printPrimeNumber(int number, int threadId) {
     lock_guard<mutex> lock(coutMutex);
     cout << number << " is a prime number, found by thread: " << threadId << endl;
-}
+} */
 
 
 bool isPrime(int number) {
     if (number <= 1) return false;
     if (number <= 3) {
+        lock_guard<mutex> lock(coutMutex);
+        cout << number << " is a prime number, found by thread: " << this_thread::get_id() << endl;
         return true;
         }
 
@@ -28,7 +30,8 @@ bool isPrime(int number) {
     for (int i = 5; i * i <= number; i = i + 6) {
         if (number % i == 0 || number % (i + 2) == 0) return false;
     }
-
+    lock_guard<mutex> lock(coutMutex);
+    cout << number << " is a prime number, found by thread: " << this_thread::get_id() << endl;
     return true;
 }
 
@@ -36,7 +39,7 @@ void sumPrimes(int start, int end, int threadId) {
     for (int i = start; i < end; i++) {
         if (isPrime(i)) {
             sumMutex.lock();
-            printPrimeNumber(i, threadId);
+            // printPrimeNumber(i, threadId);
             sum += i;
             sumMutex.unlock();
         }
@@ -48,18 +51,22 @@ int main() {
 
     vector<thread> threads;
 
-    threads.emplace_back(sumPrimes, 0, 25000, 1);
+     threads.emplace_back(sumPrimes, 0, 25000, 1);
     threads.emplace_back(sumPrimes, 25001, 50000, 2);
     threads.emplace_back(sumPrimes, 50001, 75000, 3);
     threads.emplace_back(sumPrimes, 75001, 100000, 4);
 
-    threads[0].join();
+    /* threads.push_back(thread(sumPrimes, 0, 25000, 1));
+    threads.push_back(thread(sumPrimes, 25001, 50000, 2));
+    threads.push_back(thread(sumPrimes, 50001, 75000, 3));
+    threads.push_back(thread(sumPrimes, 75001, 100000, 4)); */
+
+/*     threads[0].join();
     threads[1].join();
     threads[2].join();
-    threads[3].join();
-    //for (auto& t : threads) t.join();
+    threads[3].join(); */
+    for (auto& t : threads) t.join();
     
-
     auto endTime = chrono::high_resolution_clock::now();
 
     auto duration = chrono::duration_cast<chrono::milliseconds>(endTime - startTime);
